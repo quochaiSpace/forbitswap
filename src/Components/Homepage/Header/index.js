5142f55e8829a39008ce23a7e8e8732d798c186a
Quốc Hải <space.quochai@gmail.com>
2023-04-05 16:31:56 +0700

    layout structure

---
+++ src/Components/Homepage/Header/index.js
@@ -0,0 +1,10 @@
+import styles from "./index.module.scss"
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            Header
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+
+const Homepage = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
+
+export default Homepage;
